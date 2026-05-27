7f0cbc6356abb872da99f0ef2d2b8ba19665ca31
mdrabil <ermdrabil@gmail.com>
2026-05-27 09:43:59 +0530

    last-commit

--- index.js
+++ index.js
@@ -1,11 +1,87 @@
+
+
+
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+
+// import connectDB from "./config/db.js";
+// import routes from "./routes/index.js";
+
+// dotenv.config();
+
+// connectDB();
+
+// const app = express();
+
+// /* ==============================
+//    CORS
+// ============================== */
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://k63zgbxb-3000.inc1.devtunnels.ms",
+//       "http://localhost:3001",
+//       "https://nukkadnatak.com",
+//       "http://nukkadnatak.com",
+//       "https://admin.nukkadnatak.com",
+//       "http://admin.nukkadnatak.com",
+//     ],
+//     methods: [
+//       "GET",
+//       "POST",
+//       "PUT",
+//       "DELETE",
+//       "OPTIONS",
+//     ],
+//     allowedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//     ],
+//     credentials: true,
+//   })
+// );
+
+
+
+// /* ==============================
+//    MIDDLEWARE
+// ============================== */
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("API Running Successfully");
+// });
+
+// /* ==============================
+//    API ROUTES
+// ============================== */
+
+// app.use("/api", routes);
+
+// /* ==============================
+//    SERVER
+// ============================== */
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(
+//     `Server Running On Port ${PORT}`
+//   );
+// });
+
+
+
+
 import express from "express";
-
 import dotenv from "dotenv";
-
 import cors from "cors";
 
 import connectDB from "./config/db.js";
-
 import routes from "./routes/index.js";
 
 dotenv.config();
@@ -15,18 +91,19 @@ connectDB();
 const app = express();
 
 /* ==============================
-   MIDDLEWARE
+   CORS
 ============================== */
 
 app.use(cors());
 
+/* ==============================
+   MIDDLEWARE
+============================== */
+
 app.use(express.json());
 
-
 app.get("/", (req, res) => {
-  res.send(
-    "API Running Successfully"
-  );
+  res.send("API Running Successfully");
 });
 
 /* ==============================
@@ -39,11 +116,8 @@ app.use("/api", routes);
    SERVER
 ============================== */
 
-const PORT =
-  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server Running On Port ${PORT}`
-  );
+  console.log(`Server Running On Port ${PORT}`);
 });
